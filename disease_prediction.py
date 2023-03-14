16b2fab48be90494e6ace459c645d22b849c269c
Mohamed Emam <mohamedelbanna141@gmail.com>
2023-03-14 14:24:17 +0200

    app run added

--- disease_prediction.py
+++ disease_prediction.py
@@ -104,6 +104,9 @@ CORS(app)
 def hello_world():
     return "Hello, World!"
 
+if __name__=="__main__":
+    app.run(debug=False,host='0.0.0.0')
+
 @app.route('/symptoms')
 def get_symptoms():
     return jsonify(symptoms)
